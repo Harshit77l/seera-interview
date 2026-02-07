e23bea3fcfd797c405f7ca50f15e548a8a29aad4
Harshit Lohar <sl3398865@gmail.com>
2026-02-07 16:05:01 +0530

    Phase 2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
-// import InterviewCard from "@/components/InterviewCard";
+import InterviewCard from "@/components/InterviewCard";
 
 import { getCurrentUser } from "@/lib/actions/auth.action";
 // import {
@@ -63,12 +63,12 @@ async function Home() {
           ) : (
             <p>You haven&apos;t taken any interviews yet</p>
           )}
-        </div>
+        </div> */}
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take Interviews</h2>
-
+{/* 
         <div className="interviews-section">
           {hasUpcomingInterviews ? (
             allInterview?.map((interview) => (
